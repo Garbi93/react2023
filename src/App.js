a3cc67ebe0f9ed81109d885cac279c5645b486a7
Garbi93 <iseung2093@gmail.com>
2023-10-31 11:40:11 +0900

    10/31 component, 주석, jsx 사용법

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@
 function App() {
   return (
     <div>
-
+        <h1>hello react</h1>
+        <h2>HI REACT</h2>
+        <MyComp />
     </div>
   );
+}
+
+// built-in component는 소문자로 시작
+// component 명은 대문자로 시작
+// 우리가 작성한 component는 return문을 가져야함
+// return 값은 보통 JSX 코드
+
+function MyComp() {
+    // return 에 jsx 코드 작성 가능
+    // 꼭 하나의 root tag가 존재해야 함
+    // 여러개의 root tag를 사용하려면 div 같은 태그로 감싸서 사용해야한다.
+    // retrun 값이 여러줄이면 ()로 감싸기
+    // root tag가 필요없다면 fragment 사용 가능 <></> 태그명이 없는 태그
+
+    // jsx에서 주석 사용 법
+    //{/* */} 이렇게 사용한다.
+    return (
+        <>
+            {/* fragment 는 <> </> 로 구성된 빈 태그 */}
+            <h1>HI COMPONENT</h1>
+            <h2>hello</h2>
+        </>
+    );
 }
 
 export default App;
