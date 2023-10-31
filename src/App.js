7bdc91ed0a1365f943fb7e531cb9ec2f7231bf02
Garbi93 <iseung2093@gmail.com>
2023-10-31 12:14:44 +0900

    10/31 component 연습, 작성완료

--- src/App.js
+++ src/App.js
@@ -2,9 +2,44 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+      <MyComponent2 />
+      {/*
+        <p>lorem ipsum alkdjf alkdjf </p>
+        <ul>
+            <li>lorem</li>
+            <li>ipusm</li>
+            <li>dolor</li>
+        </ul>
+      */}
     </div>
   );
+}
+
+function MyComponent1() {
+  return (
+      <div>
+          <h1>Hello Component</h1>
+      </div>
+  );
+}
+
+function MyComponent2() {
+    return (
+        <>
+            <p>lorem ipusm alkdjf alkdjf</p>
+            <ul>
+                <li>lorem</li>
+                <li>ipusm</li>
+                <li>dolor</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;
