965a32092deedd30e7bc9c0c4ae88155785fa6e5
Garbi93 <iseung2093@gmail.com>
2023-11-01 16:40:18 +0900

    11/01 module을 이용하여 여러 클래스 명을 할당하는 방법 연습 template literal 과 Array join사용함

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -5,7 +5,12 @@ export function MyText() {
       {/* red 배경색, white 글자색 */}
       {/* src/style/mystyle.module.css 파일 생성 */}
       {/* MyText.js 에서 mystyle.module.css import 하기 */}
-      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${style.error} ${style.special} ${style.coffee}`}>
+        Lorem ipsum dolor sit amet.1
+      </h1>
+      <h1 className={[style.error, style.special, style.coffee].join(" ")}>
+        Lorem ipsum dolor sit amet.2
+      </h1>
     </div>
   );
 }
