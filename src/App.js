026d68f9354b478e58255982cef2ba350251e52d
Garbi93 <iseung2093@gmail.com>
2023-11-01 15:47:57 +0900

    11/01 보통의 css 사용시 동일한 className을 사용하는 경우가 생김

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // style 적용 방법
-// 보통 css 파일
+// 보통 css 파일 => 같은 이름의 class 를 사용 할 수도 있어서 module로 사용한다.
 // css module 사용
 // chakraUI css library
 
 import { MyComp } from "./component/MyComp";
 import { MyBox } from "./component/MyBox";
+import { MyContainer } from "./component/MyContainer";
 
 function App() {
   return (
     <>
       <MyComp />
       <MyBox />
+      <MyContainer />
     </>
   );
 }
