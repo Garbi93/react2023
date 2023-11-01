c580905119381acbfab177d9d2c906c40dd7eff2
Garbi93 <iseung2093@gmail.com>
2023-11-01 11:33:43 +0900

    11/01 props의 구조분해 할당에 기본값 주는 방법 연습완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+function MyComp({
+  m = "30px",
+  p = "20px",
+  color = "brown",
+  bg = "skyblue",
+  text = "빈 컴포넌트가 되어버렸어...",
+}) {
+  return (
+    <div
+      style={{
+        padding: p,
+        margin: m,
+        color: color,
+        backgroundColor: bg,
+        fontWeight: "bold",
+        fontSize: "1.3rem",
+      }}
+    >
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+      <MyComp p="10px" color="gold" text="hello react" />
     </div>
   );
 }
