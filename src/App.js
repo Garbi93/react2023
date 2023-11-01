a981ee871ca13e7ea506d4c7627685e15855ad2b
Garbi93 <iseung2093@gmail.com>
2023-11-01 09:41:26 +0900

    11/01 props 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+function MyHeader(props) {
+  return (
+    <div style={{ color: props.color, backgroundColor: props.bg }}>
+      <div>
+        <h1>{props.text}</h1>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyHeader color="white" bg="black" text="내가 만든 제목" />
+      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" />
+      {/*  
+      <div style=color:"white"; bg:"black">
+        <h1>내가 만든 제목 </h1>
+      </div>
+      <div style=color:"orange"; bg:"skyblue">
+        <h1>다시 만든 제목 </h1>
+      </div>
+    */}
     </div>
   );
 }
