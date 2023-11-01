883b45d80f59988b3d631a4bc2a5389d1581cddb
Garbi93 <iseung2093@gmail.com>
2023-11-01 14:27:09 +0900

    11/01 default export의 import방법을 이용해 연습 완료

---
+++ src/component/MyContainer.js
@@ -0,0 +1,11 @@
+function MyContainer() {
+  return (
+    <div>
+      <p>MyContainer => MyBox</p>
+    </div>
+  );
+}
+
+export const person = { name: "손", address: "서울" };
+export const team = { title: "축구", score: "1:0" };
+export default MyContainer;
