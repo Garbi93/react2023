dd63eacfd5c8c88b1121345db8a1e97d7a44ac91
Garbi93 <iseung2093@gmail.com>
2023-11-01 11:13:38 +0900

    11/01 props를 구조 분해 할당하여 사용하는 방법을 활용한 추가 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
 function App() {
   return (
     <div>
+      <MyBox
+        p="10px"
+        m="10px"
+        bg="skyblue"
+        color="brown"
+        text="이게 바로props입니다."
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, bg, color, text }) {
+  return (
+    <div
+      style={{
+        padding: p,
+        margin: m,
+        color: color,
+        backgroundColor: bg,
+        fontWeight: "bold",
+        fontSize: "1.2rem",
+      }}
+    >
+      <p>{text}</p>
     </div>
   );
 }
