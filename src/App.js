a4928665f99b7ff245198f826b681b478e15d3d8
Garbi93 <iseung2093@gmail.com>
2023-11-01 10:31:55 +0900

    11/01 props를 구조 분해 할당하여 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 function App() {
   return (
     <>
-      <h1
-        className="note"
-        style={{ backgroundColor: "blue", color: "white" }}
-        title="제목요소"
-      >
-        Lorem.
-      </h1>
-      <p title="문단요소">Lorem ipsum dolor.</p>
-      <MyComp title="내 컴포넌트" name="흥민" />
-      {/* 우리가 만든 컴포넌트는 호출했을때 title이 적용 안된다 */}
-      <MyComp title="I am 컴포넌트에요" name="강인" />
+      <MyComp value="흥민" address="신촌" />
+      <MyComp value="강인" address="강남" />
     </>
   );
 }
 
-// 함수 인자로
-// 사용한 곳에서 넘긴 property들이 객체형태로 담김
-// 예) title="내 컴포넌트" => {title: "내 컴포넌트"}
-// 예2) title="내 컴포넌트" name="흥민" => {title: "내 컴포넌트", name: "흥민"}
-function MyComp(props) {
-  console.log("MyComp가 받은 props", props);
-  // property를 줄여서 props라고 부르기도 한다.
+// {value:"흥민",address:"신촌"}
+function MyComp({ value, address }) {
+  // 바로 구조분해 할당하여 props를 분해해 주기
+  // 메뉴얼에서는 props를 바로 사용하기 보단 구조분해할당 하여 사용해 주는것이 더 많았다.
   return (
     <div>
-      <h1 title={props.title}>hello {props.name}</h1>
-      {/* 넘겨받은 property를 받아서 사용도 가능하다 */}
+      <h1>
+        {value}(은)는 {address}에 산다.
+      </h1>
     </div>
   );
 }
