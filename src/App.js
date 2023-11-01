6bdb225f0b8823614239a89b02fc430b9b4677d9
Garbi93 <iseung2093@gmail.com>
2023-11-01 12:34:08 +0900

    11/01 component를 다른 파일에 작성후 import, exprot하고 한 파일에서 여러개를 export하는것을 연습완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {
+  MyElem,
+  MyNavBar,
+  MyContent,
+  country,
+  text,
+  value,
+} from "./component/MyElem";
 
 function App() {
+  // country , txt value MyElem MyNavBar MyContent
+  // 를 하나의 파일 에 작성하고 export하기
   return (
     <div>
-
+      <p>{country}</p>
+      <p>{text}</p>
+      <p>
+        {value.who} , {value.subject}
+      </p>
+      <MyElem />
+      <MyNavBar />
+      <MyContent />
     </div>
   );
 }
---
+++ src/component/MyElem.js
@@ -0,0 +1,17 @@
+function MyElem() {
+  return null;
+}
+
+function MyNavBar() {
+  return null;
+}
+
+function MyContent() {
+  return null;
+}
+
+let country = "한국";
+const text = "우와 너무 춥게 틀은듯;;;";
+let value = { who: "누구냐", subject: "추워;;;" };
+
+export { country, text, value, MyContent, MyElem, MyNavBar };
