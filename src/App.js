97c82f0d7a285f90f27316dce93ea60426e23d01
Garbi93 <iseung2093@gmail.com>
2023-11-01 11:47:52 +0900

    11/01 component를 다른 파일에 작성후 import, exprot하는 방법

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+// export된 컴포넌트(변수,함수,객체) 를 import해서 사용
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+import { MyHeader } from "./component/MyHeader";
+
 function App() {
   return (
     <>
-      <MyComp name={"흥민"} address={"신촌"} age={33} city={"서울"} />
-      <MyComp address={"강남"} age={22} city={"부산"} />
-      <MyComp address={"강남"} city={"부산"} />
-      <MyComp name="희찬" age={66} address="파리" city="프랑스" />
+      <MyHeader />
+      <MyComp />
+      <MyElem />
     </>
-  );
-}
-
-function MyComp({ name = "*기본값 할당 가능*", address, age = 100, city }) {
-  return (
-    <div>
-      <p>
-        {name}은 {age}살 이고 {address}, {city}에 산다
-      </p>
-    </div>
   );
 }
 
---
+++ src/component/MyHeader.js
@@ -0,0 +1,7 @@
+export function MyHeader() {
+  return (
+    <div>
+      <h1>너무 긴 코드가 작성 됨 ...</h1>
+    </div>
+  );
+}
