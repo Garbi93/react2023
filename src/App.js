2040342ce636b0c6966e41b931a4035542fb19c7
Garbi93 <iseung2093@gmail.com>
2023-11-01 10:12:20 +0900

    11/01 props2에서 빌트인 컴포넌트와 내 컴포넌트간 property 사용 연습완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
 function App() {
   return (
     <div>
+      <MyText
+        value={"내가만든 props"}
+        style={{
+          color: "skyblue",
+          backgroundColor: "orange",
+        }}
+        title={"신기하네"}
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style} title={props.title}>
+      <p>{props.value}</p>
     </div>
   );
 }
