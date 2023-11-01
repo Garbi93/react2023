73a2f7b70e4c074b1261930d4dfffc79e172fd47
Garbi93 <iseung2093@gmail.com>
2023-11-01 16:21:24 +0900

    11/01 className 의 중복발생 했을때 module로 해결하는 방법 연습

---
+++ src/component/MyBlock.js
@@ -0,0 +1,9 @@
+import style from "../css/yourstyle.module.css";
+
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,11 +1,11 @@
-import "../css/mystyle.css";
+import style from "../css/mystyle.module.css";
 export function MyText() {
   return (
     <div>
       {/* red 배경색, white 글자색 */}
-      {/* src/style/mystyle.css 파일 생성 */}
-      {/* MyText.js 에서 mystyle.css import 하기 */}
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      {/* src/style/mystyle.module.css 파일 생성 */}
+      {/* MyText.js 에서 mystyle.module.css import 하기 */}
+      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
