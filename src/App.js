1789e6c294b425843daf5d7d921a41429dd5517b
Garbi93 <iseung2093@gmail.com>
2023-11-01 09:31:19 +0900

    11/01 props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 function App() {
-  const myStyle = {
-    color: "blue",
-    backgroundColor: "gold", // lowerCamelCase
-    fontSize: "40px",
-    textAlign: "center",
-  }; // js object
   return (
     <>
-      {/* style 속성은 객체로 주어야 함 */}
-      <div style={myStyle}>Lorem ipsum dolor.</div>
-      <hr />
-      <div
-        style={{ color: "red", backgroundColor: "yellow", fontSize: "30px" }}
+      <h1
+        className="note"
+        style={{ backgroundColor: "blue", color: "white" }}
+        title="제목요소"
       >
-        Lorem ipsum dolor.
-      </div>
+        Lorem.
+      </h1>
+      <p title="문단요소">Lorem ipsum dolor.</p>
+      <MyComp title="내 컴포넌트" name="흥민" />
+      {/* 우리가 만든 컴포넌트는 호출했을때 title이 적용 안된다 */}
+      <MyComp title="I am 컴포넌트에요" name="강인" />
     </>
+  );
+}
+
+// 함수 인자로
+// 사용한 곳에서 넘긴 property들이 객체형태로 담김
+// 예) title="내 컴포넌트" => {title: "내 컴포넌트"}
+// 예2) title="내 컴포넌트" name="흥민" => {title: "내 컴포넌트", name: "흥민"}
+function MyComp(props) {
+  console.log("MyComp가 받은 props", props);
+  // property를 줄여서 props라고 부르기도 한다.
+  return (
+    <div>
+      <h1 title={props.title}>hello {props.name}</h1>
+      {/* 넘겨받은 property를 받아서 사용도 가능하다 */}
+    </div>
   );
 }
 
