79261e2bd54435913b8b277aedfd9dd6b1b36e9d
Garbi93 <iseung2093@gmail.com>
2023-11-01 10:45:47 +0900

    11/01 props를 구조 분해 할당하여 사용하는 방법을 활용한 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+function MyElem({ name, age, team }) {
+  return (
+    <div style={{ border: "2px solid black", width: "300px" }}>
+      <p style={{ color: "blue", fontWeight: "bold" }}>이름 은 {name}</p>
+      <p>나이 는 {age}</p>
+      <p>활동 팀 명은 {team}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-
+    <div style={{ display: "flex" }}>
+      <MyElem name="흥민" age={44} team="토트넘" />
+      <MyElem name="강인" age={33} team="파리" />
     </div>
   );
 }
