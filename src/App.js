b63dfee54e764f376bfce921b16e65e1bec09fef
Garbi93 <iseung2093@gmail.com>
2023-11-01 15:27:15 +0900

    11/01 보통의 css 파일 사용하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-// 우연히 임포트한 명이 동일할 때에는 어떻게 될까??
-// 내가 만든 파일이 아니라면 별칭을 부여 할 수 있다
-import MyBox, { address } from "./component/MyBox";
-import MyElem, { address as myAddress } from "./component/MyElem";
+// style 적용 방법
+// 보통 css 파일
+// css module 사용
+// chakraUI css library
 
-// as로 named export 값을 import할 때 별칭을 줄 수 있음
-// 같은 별칭을 사용할때에 오류를 해결 하기 위해
+import { MyComp } from "./component/MyComp";
+import { MyBox } from "./component/MyBox";
 
 function App() {
   return (
     <>
-      <h1>{address}</h1>
-      <h1>{myAddress}</h1>
-      <MyElem />
+      <MyComp />
       <MyBox />
     </>
   );
--- src/component/MyElem.js
+++
@@ -1,9 +0,0 @@
-export function MyElem() {
-  return (
-    <div>
-      <p>THIS IS MTELEM</p>
-    </div>
-  );
-}
-
-export const address = "강남";
