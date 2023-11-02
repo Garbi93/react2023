14f147cbd48b7b47e32614e8dd9ee29ef82d0d38
Garbi93 <iseung2093@gmail.com>
2023-11-02 15:15:35 +0900

    11/02 Select 사용

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>이름을 한글로만 입력하세요</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={5}>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>하나만 선택하세요.</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Dignissimos.</Radio>
-                <Radio value="value4">Nobis.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box>
-          <FormControl>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem.</Checkbox>
-                <Checkbox colorScheme="green">Repellendus?</Checkbox>
-                <Checkbox colorScheme="blue">Qui.</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"}>
+        <FormControl>
+          <Select placeholder="선택하세요" multiple h={"120px"}>
+            <option value="value1">Lorem.</option>
+            <option value="value2">Eum.</option>
+            <option value="value3">Quia!</option>
+            <option value="value4">Atque.</option>
+            <option value="value5">Et.</option>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 }
