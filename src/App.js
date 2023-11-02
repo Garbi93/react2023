86e0dd5a7781b97a352ecd9ed2ac4c7210b95999
Garbi93 <iseung2093@gmail.com>
2023-11-02 10:19:25 +0900

    11/02 charkra UI를 이용한 Box컴포넌트, color프로퍼티 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
+      <Box bgColor="red">Lorem ipsum dolor.</Box>
+      <Box bgColor="blue">Blanditiis magnam, quo.</Box>
+      <Box bgColor="blue.50">Ex fugit, quae.</Box>
+      <Box bgColor="blue.100">Asperiores, provident quam?</Box>
+      <Box bgColor="blue.900">Possimus, suscipit, tempore?</Box>
+      <Box bgColor="gray">Perferendis, perspiciatis, quidem?</Box>
+      <Box bgColor="linkedin.50">At, cum, illum!</Box>
+      <Box bgColor="silver">Error, nisi, numquam!</Box>
+      <Box bgColor="gold">Nemo, quae, quos.</Box>
+      <Box bgColor="violet">Accusamus nobis, temporibus?</Box>
+    </>
+  );
 }
 
 export default App;
