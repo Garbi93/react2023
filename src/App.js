b5af5f748a65e01afc6f125f916aefb28361a11a
Garbi93 <iseung2093@gmail.com>
2023-11-03 10:54:53 +0900

    11/03 함수명 작성관습에 관한 연습

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import { Button } from "@chakra-ui/react";
 
-function MyComp({ children, executeClick }) {
-  return (
-    <div>
-      <Button onClick={executeClick}>{children}</Button>
-    </div>
-  );
-}
-
 function App() {
-  function func1() {
-    console.log("func1 실행");
-  }
-
-  let func2 = () => {
-    console.log("arrow function 실행 111");
+  // 함수명 작성 관습
+  // handle이벤트명 형식으로 작성
+  let handleClick = () => {
+    console.log("second");
   };
 
+  function handleMouseEnter() {
+    console.log("third");
+  }
+
+  let handleMouseLeave = () => {
+    console.log("4th");
+  };
   return (
     <div>
-      <MyComp executeClick={func1}>Button1</MyComp>
-      <MyComp executeClick={func2}>Button2</MyComp>
-      <MyComp
-        executeClick={() => {
-          console.log("arrow function 직접 넣어서 실행 2222");
-        }}
-      >
-        Button3
-      </MyComp>
-      <MyComp
-        executeClick={() => console.log("한줄일때 중괄호 생략해서 출력3333")}
-      >
-        Button4
-      </MyComp>
+      <Button onClick={() => console.log("first")}>button1</Button>
+      <Button onClick={handleClick}>button2</Button>
+      <Button onMouseEnter={handleMouseEnter}>button3</Button>
+      <Button onMouseLeave={handleMouseLeave}>button4</Button>
     </div>
   );
 }
