6bf840edd9d7b979a7c1b7312bfae4b3e3e6966c
Garbi93 <iseung2093@gmail.com>
2023-11-03 15:47:01 +0900

    11/03 예제) 2개의 usestate를 사용해서 dom 변경하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Box, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  let result = 0;
+  const [result1, setResult1] = useState(0);
+  const [result2, setResult2] = useState(0);
+
+  function handleChangeResult1(e) {
+    setResult1(Number(e.target.value));
+    console.log(e.target.value);
+  }
+  function handleChangeResult2(e) {
+    setResult2(Number(e.target.value));
+    console.log(e.target.value);
+  }
+
+  result = result1 + result2;
+  return (
+    <>
+      <Box display={"flex"}>
+        <Input type="number" onChange={handleChangeResult1} /> +
+        <Input type="number" onChange={handleChangeResult2} /> =
+        <Text colorScheme="yellow">{result}</Text>
+      </Box>
+    </>
+  );
 }
 
 export default App;
