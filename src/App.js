4cfce1930ffc87dceda4bac53d9575b6a36d7396
Garbi93 <iseung2093@gmail.com>
2023-11-03 12:33:34 +0900

    11/03 event.preventDefault() : 기본기능 취소하고 다른일 시키기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+
+  // a 태그를 클릭시 네이버로 이동 시키기
+  // preventDefault를 사용 하여 이동 시켜보기
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;
