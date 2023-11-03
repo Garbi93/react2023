01a97837fa158b2a12707a2cc198c8dac98c7436
Garbi93 <iseung2093@gmail.com>
2023-11-03 10:31:15 +0900

    11/03 함수를 props로 전달하기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton({ children, executeClick }) {
+  return <Button onClick={executeClick}>{children}</Button>;
+}
+
+// 드개의 버튼 만들기
+// 첫번째 버튼은 클릭하면 콘솔에 "hello"
+// 두번째 버튼은 클릭하면 콘솔에 "greeting"
+
 function App() {
-  return <></>;
+  function func1() {
+    console.log("hello");
+  }
+  function func2() {
+    console.log("greeting");
+  }
+  return (
+    <>
+      <MyButton executeClick={func1} value={"hello"}>
+        헬로
+      </MyButton>
+      <MyButton executeClick={func2} value={"hello"}>
+        그리팅
+      </MyButton>
+    </>
+  );
 }
 
 export default App;
