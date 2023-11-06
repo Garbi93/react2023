d12684f326c52fac66a1f45b98d8b744aac20542
Garbi93 <iseung2093@gmail.com>
2023-11-06 10:37:32 +0900

    11/06 상태가 같은지 아닌지 판단 re-render를 결정할 때에, 객체 참조값을 사용함

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
+  // set... 메소드로 상태를 변경할 수 있음
+  // 이때에 react는 상태가 변경된 유무를 해당 값이 같은지 아닌지로 판단하여 re-render를 결정한다.
   const [number, setNumber] = useState(0);
+  const [numberObject, setNumberObject] = useState({ number: 0 });
 
-  if (number > 5) {
-    const [text, setText] = useState("");
+  function handleNumberObjectChange() {
+    numberObject.number = numberObject.number + 1;
+    setNumberObject(numberObject);
   }
 
-  return <div></div>;
+  return (
+    <div>
+      <Box>
+        <Button
+          onClick={() => {
+            setNumber(number + 1);
+          }}
+        >
+          number 변경
+        </Button>
+        <Text>{number}</Text>
+      </Box>
+      <Box>
+        <Button onClick={handleNumberObjectChange}>number 객체 변경</Button>
+        <Text>{numberObject.number}</Text>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
