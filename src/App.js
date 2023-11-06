07e7d817d48f3fb3ffa87c7e67fe4e9c306c9d82
Garbi93 <iseung2093@gmail.com>
2023-11-06 10:18:12 +0900

    11/06 state 사용의 값 변경 말고 다른것을 변경하는 것의 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { Box, Button, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [text, setText] = useState("hello");
-  console.log(text);
+  const [number, setNumber] = useState(0);
+  console.log(number);
 
-  function handleButtonClick() {
-    // text라는 state 값 변경
-    setText("greeting");
-  }
-
+  const index = number % 3;
   return (
     <div>
-      <Button onClick={handleButtonClick}>상태변경!</Button>
-      <p>{text}</p>
+      <Button onClick={() => setNumber(number + 1)}>다음</Button>
+      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
+        <Text>message1</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
+        <Text>message2</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
+        <Text>message3</Text>
+      </Box>
     </div>
   );
 }
