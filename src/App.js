5f97f51b38f49a73fcc532371d063c940b51bb3f
Garbi93 <iseung2093@gmail.com>
2023-11-07 10:44:27 +0900

    11/07 다른 컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용해야함,

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+
+function MyInput({ address, onChange }) {
+  function handleInputChange(e) {
+    onChange(e.target.value);
+  }
+
+  return (
+    <Box>
+      <Input value={address} onChange={handleInputChange} />
+    </Box>
+  );
+}
+
+function MyText({ address }) {
+  return (
+    <Box>
+      <Text>{address}</Text>
+    </Box>
+  );
+}
+
+function App() {
+  const [address, setAddress] = useState("");
+  function handleInputChange(text) {
+    setAddress(text);
+  }
+  return (
+    <div>
+      <MyInput address={address} onChange={handleInputChange} />
+      <MyText address={address} />
+    </div>
+  );
+}
+
+export default App;
