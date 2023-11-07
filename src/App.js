150dce6fb2680b1314b51788a7d35fcfb95e8392
Garbi93 <iseung2093@gmail.com>
2023-11-07 12:33:17 +0900

    11/07 prop drilling을 Context로 사용 하여 여러 부모를 거치지 않고 조상님한테 바로 받아 사용 하기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Button, Text } from "@chakra-ui/react";
 
-function ChildComp({ onClick }) {
-  return <Button onClick={onClick}>클릭!!!</Button>;
+function CComp() {
+  // 3. context 사용하기 : useContext(Context이름);
+  const message = useContext(MessageContext);
+  // 4. 받은메세지 사용하기
+  return <Text> 받은 메세지 : {message} </Text>;
 }
 
-function SomeComp({ onClick }) {
-  return <ChildComp onClick={onClick} />;
+function BComp() {
+  return <CComp />;
 }
 
-function OtherChildComp({ message }) {
-  return <Text>{message}</Text>;
+function AComp() {
+  return <BComp />;
 }
 
-function OtherComp({ message }) {
-  return <OtherChildComp message={message} />;
-}
+function App(props) {
+  const [message, setMessage] = useState("");
 
-function App() {
-  const [message, setMessage] = useState("초기 메세지");
+  // message state를 CComp에 전달 하기 -> context를 사용 하여
+  // 1. context 만들기 : createContext();
+  // 2. context에 state 넣기 : <Context.Provider value={state}></Context.Provider>
+  // 3. tree안에 context사용하기
+
   return (
     <div>
-      <SomeComp onClick={() => setMessage("다른메세지!!!")} />
-      <OtherComp message={message} />
+      <Button onClick={() => setMessage("바꾼 메세지!!!")}>
+        메세지 바꾸기
+      </Button>
+      {/* 2. context에 state 넣기 */}
+      <MessageContext.Provider value={message}>
+        <AComp />
+      </MessageContext.Provider>
     </div>
   );
 }
+
+// 1. context 만들기
+// context 이름은 : (하는일)...Context (예-> MemberContext, MessageContext) 로 지어주면 좋다.
+// 대문자로 시작
+const MessageContext = createContext(null);
 
 export default App;
