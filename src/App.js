0ac0f66487b9d3e9bd94397fa2fc92234a001543
Garbi93 <iseung2093@gmail.com>
2023-11-08 09:29:44 +0900

    11/08 useEffect 의 두번째 파라미터는 첫번째 파라미터 (setup, effect)를 실행 시키는 트리거가 되는 값.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Button, Input, Text } from "@chakra-ui/react";
 
 function App() {
-  const [myState, setMyState] = useState("");
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
 
+  // 첫번째 파라미터(setup) : The function with your Effect's logic. (부수적인 작용이 있는 함수)
+  // 두번째 파라미터 : setup을 실행시키는 값의 나열(배열),
+  // 아무것도 나열하지 않으면(빈 배열) 첫 렌더링 때만 실행시키도록 한다.
+
+  // 두번째 파라미터에는 state, prop, 등 다양한 것들이 들어갈 수 있다.
   useEffect(() => {
-    axios
-      .get("/api/main1/sub1")
-      .then((response) => response.data)
-      .then((data) => setMyState(data))
-      .catch((error) => console.log(error));
-  }, []);
-  return <div>{myState}</div>;
+    console.log("effect 함수 실행됨");
+  }, [number, text]);
+
+  return (
+    <div>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+  );
 }
 
 export default App;
